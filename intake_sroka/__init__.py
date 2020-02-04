a03b108e7f9c725735bbb5a17d8d718ad6283375
Dan Allan <dallan@bnl.gov>
2020-02-04 14:48:49 -0500

    Add support for new driver discovery mechanism.

--- intake_sroka/__init__.py
+++ intake_sroka/__init__.py
@@ -2,4 +2,6 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import intake  # Import this first to avoid circular imports during discovery.
+del intake
 from .sources import *
